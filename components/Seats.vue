0b0485e98efadee262b824e7df6a4eb3c47bfe73
Jakz Aizzat <jakzaizzat@gmail.com>
2019-06-18 09:17:49 +0800

    add seats left

--- components/Seats.vue
+++ components/Seats.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="seats">
-    <div class="lane flex flex-wrap">
-      <div v-for="lane in lanes" class="flex-1 flex flex-col flex-wrap mx-2" :key="lane.length">
+    <div class="lanes">
+      <div v-for="lane in lanes" class="lane" :key="lane.length">
         <div v-for="(item,index) in lane" :key="index" class="flex justify-center">
           <div class="bg-red-200 m-1" v-for="(value, index) in item" :key="index">
             <Seat>{{ value }}</Seat>
@@ -32,7 +32,11 @@ export default {
   @apply flex flex-1;
 }
 
+.lanes {
+  @apply bg-blue-200 w-full py-3  flex flex-wrap rounded shadow;
+}
+
 .lane {
-  @apply bg-blue-200 w-full py-3;
+  @apply flex-1 flex flex-col flex-wrap mx-2;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -20,10 +20,11 @@
             for="grid-password"
           >a 2D array that represents the rows and columns for available seats</label>
           <input
-            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-1 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             type="text"
             v-model="form.array"
           >
+          <p class="text-gray-600 text-xs mb-4">Example of array : [ [2,3], [3,4], [3,2], [4,3] ]</p>
         </div>
 
         <button
@@ -44,7 +45,27 @@
           <span>Generate</span>
         </button>
       </form>
-      <div v-if="lanes.length>0">
+
+      <div v-if="lanes.length > 0">
+        <div
+          class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 mb-4"
+          role="alert"
+        >
+          <p>
+            Total seats available -
+            <span class="font-bold">{{seatReport.available}}</span>
+          </p>
+          <p v-if="seatReport.surplus < 0">
+            Number of passengers didn't get a sit -
+            <span
+              class="font-bold"
+            >{{ seatReport.surplus | positive }}</span>
+          </p>
+          <p v-else>
+            Seats Available -
+            <span class="font-bold">{{ seatReport.surplus }}</span>
+          </p>
+        </div>
         <Seats :lanes="lanes"/>
       </div>
     </div>
@@ -52,46 +73,68 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 import Seats from '~/components/Seats'
 
 export default {
   components: {
-    Logo,
     Seats
   },
   data() {
     return {
-      starting: 0,
-      structures: {
-        counter: 0,
-        data: ''
-      },
       lanes: [],
       form: {
         passengers: 0,
         array: ''
+      },
+      seatReport: {
+        available: 0,
+        surplus: 0
       }
     }
   },
   methods: {
     calculate() {
-      this.structures.counter++
-      this.structures.data = JSON.parse(this.form.array.replace(/'/g, '"'))
-      this.$axios
-        .$post(`/`, this.form)
-        .then(res => {
-          console.log(res)
-          this.lanes = res
+      try {
+        var o = JSON.parse(this.form.array.replace(/'/g, '"'))
+      } catch (e) {
+        alert('invalid Array')
+        console.log(e)
+        return
+      }
 
-          // this.form = {
-          //   passengers: 0,
-          //   array: ''
-          // }
-        })
-        .catch(err => {
-          console.log(err)
-        })
+      var parseArr = JSON.parse(this.form.array.replace(/'/g, '"'))
+      if (parseArr[0].constructor === Array) {
+        this.$axios
+          .$post(`/`, this.form)
+          .then(res => {
+            this.lanes = res
+            this.passengersReport()
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      } else {
+        alert('This is not 2D Array')
+        return
+      }
+    },
+    passengersReport() {
+      this.seatReport = {
+        available: 0,
+        surplus: 0,
+        left: 0
+      }
+      var seatArr = JSON.parse(this.form.array.replace(/'/g, '"'))
+      seatArr.forEach(seat => {
+        this.seatReport.available += seat[0] * seat[1]
+      })
+
+      this.seatReport.surplus = this.seatReport.available - this.form.passengers
+    }
+  },
+  filters: {
+    positive(value) {
+      return abs(value)
     }
   }
 }
